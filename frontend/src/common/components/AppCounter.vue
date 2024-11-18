63588c62adfd44a12aead6183f0eb6c7243917ff
Алексей <malyshevalexey96@gmail.com>
2024-11-18 21:40:48 +0300

    Счетчик в отдельный компонент и перенос стилей

---
+++ frontend/src/common/components/AppCounter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :class="`counter counter--${color} ${component}__counter`">
+    <button type="button" class="counter__button counter__button--minus" @click="counter > 0? counter-- : ''">
+      <span class="visually-hidden">Меньше</span>
+    </button>
+    <input type="text" name="counter" class="counter__input" :min="0" v-model="counter">
+    <button type="button" class="counter__button counter__button--plus" @click="counter ++">
+      <span class="visually-hidden">Больше</span>
+    </button>
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  color: {
+    type: String,
+    required: false,
+    default: 'orange'
+  },
+  component: {
+    type: String,
+    required: false,
+    default: 'ingredients'
+  },
+})
+
+const counter = defineModel({type: Number, default: 0});
+
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/counter';
+</style>
