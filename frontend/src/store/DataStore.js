e1731c727aa83bc3fdff1cdded646d5fbaf268ce
Алексей <malyshevalexey96@gmail.com>
2024-12-03 15:50:04 +0300

    геттер

--- frontend/src/store/DataStore.js
+++ frontend/src/store/DataStore.js
@@ -1,13 +1,28 @@
-import { defineStore } from 'pinia';
-
+import {defineStore} from 'pinia';
+import {computed, ref} from "vue";
+import {calculatePrice} from "@/common/helpers/calculatePrice";
 export const useDataStore = defineStore('data', () =>{
     // State
-
+    const pizzaCart = ref({
+        dough: {},
+        ingredients: [],
+        size: {},
+        sauce: {},
+        name: ''
+    })
     // Actions
 
     // Getters
 
-    return {
+    const getPrice = computed(() => {
+        const price = Object.values(pizzaCart.value).reduce((acc, item) => {
+            return acc += calculatePrice(item)
+        }, 0)
+        return price * (pizzaCart.value.size.multiplier ?? 1)
+    })
 
+    return {
+        getPrice,
+        pizzaCart
     }
 });
